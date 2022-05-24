3ccdd6af896b4dafa735f7e8334022f510eddf3d
Andrea Allen <andrea2allen@gmail.com>
2022-05-24 17:02:48 -0400

    fix file name again

--- compressnets/demos.py
+++ compressnets/demos.py
@@ -8,7 +8,7 @@ class Sample:
 
     @staticmethod
     def get_sample_temporal_network():
-        FILENAME = os.path.join(os.path.dirname(__file__), 'datafiles/sample_network')
+        FILENAME = os.path.join(os.path.dirname(__file__), 'sample_network')
         with gzip.open(FILENAME, 'r') as fin:  # 4. gzip
             json_bytes = fin.read()  # 3. bytes (i.e. UTF-8)
 
