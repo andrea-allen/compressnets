8c652e3245a20e6386571d15ee0882eff60704d7
Andrea Allen <andrea2allen@gmail.com>
2022-05-24 15:51:24 -0400

    debugging relative filename

--- compressnets/demos.py
+++ compressnets/demos.py
@@ -1,13 +1,15 @@
 from compressnets.compression import *
 import gzip
 import json
+import os
 
 
 class Sample:
 
     @staticmethod
     def get_sample_temporal_network():
-        with gzip.open('./../datafiles/sample_network', 'r') as fin:  # 4. gzip
+        TEST_FILENAME = os.path.join(os.path.dirname(__file__), './../datafiles/sample_network')
+        with gzip.open(TEST_FILENAME, 'r') as fin:  # 4. gzip
             json_bytes = fin.read()  # 3. bytes (i.e. UTF-8)
 
         json_str = json_bytes.decode('utf-8')  # 2. string (i.e. JSON)
